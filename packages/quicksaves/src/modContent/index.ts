300357b18105e0d5e297109de27261bdd0e06a00
nozwock <57829219+nozwock@users.noreply.github.com>
2026-04-29 00:11:32 +0530

    Add quicksave buttons to most screens

--- packages/quicksaves/src/modContent/index.ts
+++ packages/quicksaves/src/modContent/index.ts
@@ -1,4 +1,60 @@
 import { QuickSaves } from './quicksaves';
+import { QuickSaveButtons } from './ui';
+
+// TODO: `combat` screen, awaiting on the top bar flex container getting an `id`
+
+window.modAPI.injectUI('crafting', (api, element, inject) => {
+  return inject(
+    '#backgroundImage',
+    QuickSaveButtons(api, '138px', '3px', '5px'),
+    'inline',
+  );
+});
+
+window.modAPI.injectUI('dualCultivation', (api, element, inject) => {
+  return inject(
+    '#backgroundImage',
+    QuickSaveButtons(api, '50px', '3px', '5px'),
+    'inline',
+  );
+});
+
+window.modAPI.injectUI('event', (api, element, inject) => {
+  return inject(
+    '#backgroundImage',
+    QuickSaveButtons(api, '3px', '140px', '5px'),
+    'inline',
+  );
+});
+
+// TODO: Waiting for player UI components to have an `id` to target instead of every screen they appear in.
+window.modAPI.injectUI('map', (api, element, inject) => {
+  return inject('#backgroundImage', QuickSaveButtons(api), 'inline');
+});
+window.modAPI.injectUI('location', (api, element, inject) => {
+  return inject('#backgroundImage', QuickSaveButtons(api), 'inline');
+});
+window.modAPI.injectUI('market', (api, element, inject) => {
+  return inject('#backgroundImage', QuickSaveButtons(api), 'inline');
+});
+window.modAPI.injectUI('herbField', (api, element, inject) => {
+  return inject('#backgroundImage', QuickSaveButtons(api), 'inline');
+});
+window.modAPI.injectUI('recipe', (api, element, inject) => {
+  return inject('#backgroundImage', QuickSaveButtons(api), 'inline');
+});
+window.modAPI.injectUI('auction', (api, element, inject) => {
+  return inject('#backgroundImage', QuickSaveButtons(api), 'inline');
+});
+window.modAPI.injectUI('mysticalRegion', (api, element, inject) => {
+  return inject('#backgroundImage', QuickSaveButtons(api), 'inline');
+});
+window.modAPI.injectUI('house', (api, element, inject) => {
+  return inject('#backgroundImage', QuickSaveButtons(api), 'inline');
+});
+window.modAPI.injectUI('guild', (api, element, inject) => {
+  return inject('#backgroundImage', QuickSaveButtons(api), 'inline');
+});
 
 // TODO: Add toasts for quick save/load, awaiting on a toast API or component in 0.6.53,
 window.addEventListener('keyup', (e) => {
---
+++ packages/quicksaves/src/modContent/ui.tsx
@@ -0,0 +1,62 @@
+import { Stack, SvgIcon } from '@mui/material';
+import { ModReduxAPI } from 'afnm-types';
+import { QuickSaves } from './quicksaves';
+
+function LoadIcon() {
+  return (
+    <SvgIcon>
+      <svg fill="currentColor" viewBox="0 0 24 24" stroke="currentColor">
+        <path d="M18 15v3H6v-3H4v3c0 1.1.9 2 2 2h12c1.1 0 2-.9 2-2v-3zM7 9l1.41 1.41L11 7.83V16h2V7.83l2.59 2.58L17 9l-5-5z"></path>
+      </svg>
+    </SvgIcon>
+  );
+}
+
+function SaveIcon() {
+  return (
+    <SvgIcon>
+      <svg fill="currentColor" viewBox="0 0 24 24" stroke="currentColor">
+        <path d="M18 15v3H6v-3H4v3c0 1.1.9 2 2 2h12c1.1 0 2-.9 2-2v-3zm-1-4-1.41-1.41L13 12.17V4h-2v8.17L8.41 9.59 7 11l5 5z"></path>
+      </svg>
+    </SvgIcon>
+  );
+}
+
+export function QuickSaveButtons(
+  api: ModReduxAPI,
+  left?: string,
+  bottom?: string,
+  spacing?: string,
+) {
+  left = left ?? '65px';
+  bottom = bottom ?? '0px';
+  spacing = spacing ?? '0px';
+  return (
+    <Stack
+      id="quicksaveButtons"
+      position="absolute"
+      zIndex={100}
+      left={left}
+      bottom={bottom}
+      direction="row"
+      spacing={spacing}
+    >
+      <api.components.GameIconButton
+        title="Quicksasve"
+        onClick={() => {
+          if (api.hasSave) QuickSaves.makeQuickSave();
+        }}
+      >
+        <SaveIcon />
+      </api.components.GameIconButton>
+      <api.components.GameIconButton
+        title="Load Last Quicksasve"
+        onClick={() => {
+          if (api.hasSave) QuickSaves.loadLastQuickSave();
+        }}
+      >
+        <LoadIcon />
+      </api.components.GameIconButton>
+    </Stack>
+  );
+}
