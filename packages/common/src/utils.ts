4e488323e2695a1a12e220619c41b4c71ebff843
nozwock <57829219+nozwock@users.noreply.github.com>
2026-05-09 19:01:16 +0530

    Remove old `RegisteredKeybind.code` check against `KeyboardEvent.key`
    The issue was fixed in `0.6.56`.

--- packages/common/src/utils.ts
+++ packages/common/src/utils.ts
@@ -38,13 +38,7 @@ export function matchRegisteredKeybind(action: string, event: KeyboardEvent) {
   const key = window.modAPI.utils.getRegisteredKeybindValue(action);
   return (
     key !== undefined &&
-    // FIXME: `RegisteredKeybind.code` was supposed to be equivalent to `KeyboardEvent.code` but it isn't. It's instead like
-    // `KeyboardEvent.key` but without modifiers (and possibly keyboard layout) affecting it etc, so it can't be matched
-    // against `KeyboardEvent.key` reliably either.
-    //
-    // `code === code` match is so keybinds still keep working when in the future the API gets updated to have proper
-    // value for `code`
-    (event.key === key.code || event.code === key.code) &&
+    event.code === key.code &&
     event.ctrlKey === key.ctrlKey &&
     event.altKey === key.altKey &&
     event.shiftKey === key.shiftKey
